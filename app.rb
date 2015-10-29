3f8f0f83cd0c1d5d19f55631bdb08bb2050ec875
thisdotrob <thisdotrob@gmail.com>
2015-10-29 19:35:02 +0000

    Refactor /names to use a POST-redirect

--- app.rb
+++ app.rb
@@ -1,14 +1,23 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+
+  enable :sessions
+
   get '/' do
-    erb(:index)
+    erb :index
   end
 
   post '/names' do
-    @player1_name = params[:player1_name]
-    @player2_name = params[:player2_name]
-    erb(:play)
+    session[:player1_name] = params[:player1_name]
+    session[:player2_name] = params[:player2_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player1_name = session[:player1_name]
+    @player2_name = session[:player2_name]
+    erb :play
   end
 
   run! if app_file == $0
