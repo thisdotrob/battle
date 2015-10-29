fae05ec00eb22b577a2c52ee5e48705650bc7fef
thisdotrob <thisdotrob@gmail.com>
2015-10-29 21:46:27 +0000

    Refactor controller to use global variables instead of the session

--- app.rb
+++ app.rb
@@ -10,14 +10,14 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    session[:player1_name] = params[:player1_name]
-    session[:player2_name] = params[:player2_name]
+    $player1 = Player.new(params[:player1_name])
+    $player2 = Player.new(params[:player2_name])
     redirect '/play'
   end
 
   get '/play' do
-    @player1_name = session[:player1_name]
-    @player2_name = session[:player2_name]
+    @player1_name = $player1.name
+    @player2_name = $player2.name
     @attacked = session[:attacked?]
     erb :play
   end
