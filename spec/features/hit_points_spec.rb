a88329e318a91946cd83e197fa5508d9ff3c41a6
thisdotrob <thisdotrob@gmail.com>
2015-10-29 19:47:48 +0000

    Refactor feature tests to use web_helpers.rb

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,9 +1,6 @@
 feature 'View hit points' do
   scenario 'see Player 2 hit points' do
-    visit('/')
-    fill_in :player1_name, with: 'Player1'
-    fill_in :player2_name, with: 'Player2'
-    click_button 'Play'
+    sign_in_and_play
     expect(page).to have_content 'Player2: 100HP'
   end
 end
--- spec/features/names_spec.rb
+++ spec/features/names_spec.rb
@@ -1,9 +1,6 @@
 feature 'Enter names' do
   scenario 'submitting names' do
-    visit('/')
-    fill_in :player1_name, with: 'Player1'
-    fill_in :player2_name, with: 'Player2'
-    click_button 'Play'
+    sign_in_and_play
     expect(page).to have_content 'Player1 vs. Player2'
   end
 end
