5acbdfbda00783ffb80ed2b35450044c8e41f3d5
thisdotrob <thisdotrob@gmail.com>
2015-10-29 22:18:34 +0000

    Refactor player_spec to include #attack

--- spec/features/play_spec.rb
+++ spec/features/play_spec.rb
@@ -7,12 +7,12 @@ feature '/play' do
     sign_in_and_play
     expect(page).to have_content 'Player2: 100HP'
   end
-  scenario 'Displays confirmation of attack on Player 2' do
+  scenario 'displays confirmation of attack on Player 2' do
     sign_in_and_play
     click_button('Attack')
     expect(page).to have_content 'Player1 attacked Player2!'
   end
-  scenario 'Displays reduced hitpoints after attack on Player 2' do
+  scenario 'displays reduced hitpoints after attack on Player 2' do
     sign_in_and_play
     5.times { click_button('Attack') }
     expect(page).to_not have_content 'Player2: 100HP'
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,23 +1,30 @@
 require 'player'
 
 describe Player do
-  subject(:player){ Player.new(:name) }
+  subject(:player1){ Player.new(:name) }
 
   context '#name' do
     it 'returns the name' do
-      expect(player.name).to eq (:name)
+      expect(player1.name).to eq (:name)
     end
   end
 
   context '#hitpoints' do
     it 'returns the default hitpoints' do
-      expect(player.hitpoints).to eq (Player::DEF_HITPOINTS)
+      expect(player1.hitpoints).to eq (Player::DEF_HITPOINTS)
+    end
+  end
+
+  context '#attack' do
+    it 'calls #damage on the other player' do
+      expect(player2).to receive(:damage)
+      player1.attack(player2)
     end
   end
 
   context '#damage' do
     it 'reduces hitpoints by 10' do
-      expect{player.damage}.to change(player, :hitpoints).by(-10)
+      expect{player1.damage}.to change(player1, :hitpoints).by(-10)
     end
   end
 end
