f6d23cab609050478f9a830fa21eb8ed5f4d40f3
thisdotrob <thisdotrob@gmail.com>
2015-10-29 22:03:41 +0000

    Add Player default hitpoints unit test

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,9 +2,16 @@ require 'player'
 
 describe Player do
   subject(:player){ Player.new(:name) }
-  context 'name' do
+
+  context '#name' do
     it 'returns the name' do
       expect(player.name).to eq (:name)
+    end
+  end
+
+  context '#hitpoints' do
+    it 'returns the hitpoints' do
+      expect(player.hitpoints).to eq (Player::DEF_HITPOINTS)
     end
   end
 end
