b876a4de3d69148f11f16c5b6651128d6f35c934
thisdotrob <thisdotrob@gmail.com>
2015-10-29 21:51:22 +0000

    Add /play hitpoints displayed feature test

--- spec/features/play_spec.rb
+++ spec/features/play_spec.rb
@@ -12,4 +12,10 @@ feature '/play' do
     click_button('Attack')
     expect(page).to have_content 'Player1 attacked Player2!'
   end
+  scenario 'Displays reduced hitpoints after attack on Player 2' do
+    sign_in_and_play
+    click_button('Attack')
+    expect(page).to_not have_content 'Player2: 100HP'
+    expect(page).to have_content 'Player2: 90HP'
+  end
 end
