0c6fcba98f6df40b5d83a40fcb0594cac1ea7906
thisdotrob <thisdotrob@gmail.com>
2015-10-29 22:46:55 +0000

    Add Game unit tests

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,15 +1,22 @@
 require 'game'
 
 describe Game do
-  subject(:game){ Game.new }
+  subject(:game){ Game.new(player1, player2) }
 
-  let(:player){ double(:player) }
+  let(:player1){ double(:player1) }
+  let(:player2){ double(:player2) }
+
+  context '#initialize' do
+    it 'creates two Player instances' do
+      expect(game.players).to eq ([player1, player2])
+    end
+  end
 
   context '#attack' do
     it 'calls #damage on the player' do
-      expect(player).to receive(:damage)
-      game.attack(player)
+      expect(player1).to receive(:damage)
+      game.attack
     end
   end
-  
+
 end
