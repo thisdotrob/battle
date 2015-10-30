734982302dc155ff8fba6f0061b8b43aca5b54e8
thisdotrob <thisdotrob@gmail.com>
2015-10-30 10:02:40 +0000

    Pass unit tests for Game #current_player

--- lib/game.rb
+++ lib/game.rb
@@ -2,15 +2,17 @@ require 'forwardable'
 
 class Game
   extend Forwardable
-  attr_reader :player1, :player2
+  attr_reader :player1, :player2, :current_player
 
   def_delegator :player1, :name, :player1_name
   def_delegator :player2, :name, :player2_name
   def_delegator :player2, :hitpoints, :player2_hitpoints
+  def_delegator :current_player, :name, :current_player_name
 
   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
+    set_current_player
   end
 
   def attack
@@ -20,6 +22,12 @@ class Game
 
   def attacked?
     @attacked
+  end
+
+  private
+
+  def set_current_player
+    @current_player = Kernel.rand > 0.5 ? self.player1 : self.player2
   end
 
 end
